4f3447c38517309a16b8c775599326f34e9c7e3d
AssoDePicche <samuelprado730@gmail.com>
2025-09-15 00:09:30 -0300

    feat: game.h

---
+++ game/include/game.h
@@ -0,0 +1,26 @@
+#ifndef __GAME__
+#define __GAME__
+
+#include <stdbool.h>
+
+#define WINDOW_SIZE 640
+#define WINDOW_CELL_SIZE WINDOW_SIZE / 3
+#define WINDOW_FIRST_THIRD 0
+#define WINDOW_SECOND_THIRD WINDOW_SIZE / 3
+#define WINDOW_THIRD_THIRD 2 * WINDOW_SECOND_THIRD
+
+typedef struct Game Game;
+
+Game* game_construct(void);
+
+void game_destruct(Game*);
+
+void game_draw(const Game*);
+
+void game_update(Game*);
+
+void game_input(Game*);
+
+bool is_running(const Game*);
+
+#endif
